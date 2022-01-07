8b01f461033652a9ad6ed23ad669a1401f4c2c34
oldmartijntje <oldmartijntje@gmai.com>
2022-01-07 17:17:20 +0100

    a

---
+++ test/test/Form1.cs
@@ -0,0 +1,25 @@
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
